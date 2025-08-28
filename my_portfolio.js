e791753bea2744941138f5ce5a5f9fcec9078f73
Oladeji-Stephen-Ayomide <oladejistephen012@gmail.com>
2025-08-28 12:18:21 -0700

    changed to superhero

--- my_portfolio.js
+++ my_portfolio.js
@@ -29,7 +29,7 @@
 // Replace the arguments below according to your preference.
 
 // space, scary, military, romantic, cowboy, fantasy, superhero
-favouriteMovieGenre("cowboy");
+favouriteMovieGenre("superhero");
 
 // watermelon, tomato, banana, orange, avocado, blueberry
 favouriteFruit("banana");
